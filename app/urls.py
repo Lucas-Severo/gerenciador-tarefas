4abfe73cc137e3a6053a333b34525d4f4abe57f5
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2019-09-11 13:13:42 -0400

    Alteração de rotas

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from .views.tarefa_views import *
 from .views.usuario_views import *
 
 urlpatterns = [
+    path('', listar_tarefas, name='listar_tarefas'),
     path('listar_tarefas/', listar_tarefas, name='listar_tarefas'),
     path('cadastrar_tarefa/', cadastrar_tarefa, name="cadastrar_tarefa"),
     path('editar_tarefa/<int:id>', editar_tarefa, name="editar_tarefa"),
